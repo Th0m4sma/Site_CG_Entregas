c5fb5eac639400c8ca6e996bffae3c7f85283978
Th0m4sma <thomaspirescorreia@gmail.com.br>
2025-11-29 11:35:14 -0300

    Criando Pagina Inicial - Login

---
+++ func_js/pagina_inicial.js
@@ -0,0 +1,273 @@
+// Vertex shader source code
+const vertexShaderSource = `
+    attribute vec3 a_position;
+    attribute vec3 a_color;
+    varying vec3 v_color;
+    uniform mat4 u_modelViewMatrix;
+    uniform mat4 u_viewingMatrix;
+    uniform mat4 u_projectionMatrix;
+
+    void main() {
+        gl_Position = u_projectionMatrix * u_viewingMatrix * u_modelViewMatrix * vec4(a_position,1.0);
+        v_color = a_color;
+    }
+`;
+
+// Fragment shader source code
+const fragmentShaderSource = `
+    precision mediump float;
+    varying vec3 v_color;
+    void main() {
+        gl_FragColor = vec4(v_color,1.0);
+    }
+`;
+
+function createShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error('Error compiling shader:', gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('Error linking program:', gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+        return null;
+    }
+
+    return program;
+}
+
+function setCubeVertices(side){
+  let v = side/2;
+  return new Float32Array([
+      // Front
+      v, v, v,
+      v, -v, v,
+      -v, v, v,
+      -v, v, v,
+      v, -v, v,
+      -v, -v, v,
+  
+      // Left
+      -v, v, v,
+      -v, -v, v,
+      -v, v, -v,
+      -v, v, -v,
+      -v, -v, v,
+      -v, -v, -v,
+  
+      // Back
+      -v, v, -v,
+      -v, -v, -v,
+      v, v, -v,
+      v, v, -v,
+      -v, -v, -v,
+      v, -v, -v,
+  
+      // Right
+      v, v, -v,
+      v, -v, -v,
+      v, v, v,
+      v, v, v,
+      v, -v, v,
+      v, -v, -v,
+  
+      // Top
+      v, v, v,
+      v, v, -v,
+      -v, v, v,
+      -v, v, v,
+      v, v, -v,
+      -v, v, -v,
+  
+      // Bottom
+      v, -v, v,
+      v, -v, -v,
+      -v, -v, v,
+      -v, -v, v,
+      v, -v, -v,
+      -v, -v, -v,
+  ]);
+}
+
+function setCubeColors(){
+  let colors = [];
+  let color = [];
+  color = [Math.random(),Math.random(),Math.random()];
+  for(let i=0;i<6;i++){
+    for(let j=0;j<6;j++)
+      colors.push(...color);
+  }
+
+  return new Float32Array(colors);
+}
+
+function defineCoordinateAxes(){
+    return new Float32Array([
+      -1.0, 0.0, 0.0,
+      1.0, 0.0, 0.0,
+      0.0, -1.0, 0.0,
+      0.0, 1.0, 0.0,
+      0.0, 0.0, -1.0,
+      0.0, 0.0, 1.0,
+    ]);
+}
+
+function defineCoordinateAxesColors(){
+    return new Float32Array([
+      1.0, 0.0, 0.0,  1.0, 0.0, 0.0,
+      1.0, 0.0, 0.0,  1.0, 0.0, 0.0,
+      1.0, 0.0, 0.0,  1.0, 0.0, 0.0,
+    ]);
+}
+
+function main() {
+    const canvas = document.getElementById('glCanvas');
+    const gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        console.error('WebGL not supported');
+        return;
+    }
+
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    const program = createProgram(gl, vertexShader, fragmentShader);
+    gl.useProgram(program);
+
+    const positionLocation = gl.getAttribLocation(program, 'a_position');
+    const colorLocation = gl.getAttribLocation(program, 'a_color');
+
+    const VertexBuffer = gl.createBuffer();
+    let cubeVertices = [];
+
+    const ColorBuffer = gl.createBuffer();
+    let cubeColors = [];
+    
+    const modelViewMatrixUniformLocation = gl.getUniformLocation(program,'u_modelViewMatrix');
+    const viewingMatrixUniformLocation = gl.getUniformLocation(program,'u_viewingMatrix');
+    const projectionMatrixUniformLocation = gl.getUniformLocation(program,'u_projectionMatrix');
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    let modelViewMatrix = m4.identity();
+
+
+
+
+
+
+
+
+    let extraCubes = [];
+
+    // Câmera inicial
+    let P0 = [4.0,4.0,4.0];
+    let Pref = [0.0,0.0,0.5];
+    let V = [1.0, 1.0, 1.0];
+
+    let viewingMatrix = m4.setViewingMatrix(P0,Pref,V);
+    
+    let xw_min = -1.0;
+    let xw_max = 1.0;
+    let yw_min = -1.0;
+    let yw_max = 1.0;
+    let z_near = -1.0;
+    let z_far = -8.0;
+    let projectionMatrix = m4.setOrthographicProjectionMatrix(xw_min,xw_max,yw_min,yw_max,z_near,z_far);
+
+
+
+
+    createRandomCubes(80); 
+
+
+    function createRandomCubes(n){
+        for(let i = 0; i < n; i++){
+            extraCubes.push({
+                vertices: setCubeVertices(0.18),
+                colors: setCubeColors(),
+                x: (Math.random() * 5) + 1,
+                y: (Math.random() * 5) + 1,
+                z: (Math.random() * 5) + 1,
+                angle: Math.random() * 360,
+                speed: 1.0
+            });
+        }
+    }
+
+
+    function drawCube(cubeVertices, cubeColors, tx=0, ty=0, tz=0, angle=0){
+        gl.enableVertexAttribArray(positionLocation);
+        gl.bindBuffer(gl.ARRAY_BUFFER, VertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, cubeVertices, gl.STATIC_DRAW);
+        gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+        gl.enableVertexAttribArray(colorLocation);
+        gl.bindBuffer(gl.ARRAY_BUFFER, ColorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, cubeColors, gl.STATIC_DRAW);
+        gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+        let M = m4.identity();
+        M = m4.translate(M, tx, ty, tz);
+        M = m4.yRotate(M, degToRad(angle));
+
+        gl.uniformMatrix4fv(modelViewMatrixUniformLocation,false,M);
+        gl.uniformMatrix4fv(viewingMatrixUniformLocation,false,viewingMatrix);
+        gl.uniformMatrix4fv(projectionMatrixUniformLocation,false,projectionMatrix);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6*6);
+    }
+
+
+    function drawScene(){
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        for(let c of extraCubes){
+            c.angle += c.speed;
+            drawCube(c.vertices, c.colors, c.x, c.y, c.z, c.angle);
+        }
+
+        requestAnimationFrame(drawScene);
+    }
+
+
+    drawScene();
+}
+
+function unitVector(v){ 
+    let vModulus = vectorModulus(v);
+    return v.map(function(x) { return x/vModulus; });
+}
+
+function vectorModulus(v){
+    return Math.sqrt(Math.pow(v[0],2)+Math.pow(v[1],2)+Math.pow(v[2],2));
+}
+
+function radToDeg(r) {
+  return r * 180 / Math.PI;
+}
+
+function degToRad(d) {
+  return d * Math.PI / 180;
+}
+
+window.addEventListener('load', main);
